d5846b3e568931e9de4f50d53a92a52061a25179
terrance74liang <terrance.liang74@gmail.com>
2022-07-07 00:20:22 -0400

    got nested manager list and shared value to work

--- database.py
+++ database.py
@@ -24,22 +24,22 @@ def tagger(data_csv):
 
 
 # stores text dicitonnary and eliminates named entitities, text is a pandas series
-def string_storing(text, string_store):
-    list_manager = multiprocessing.Manager()
+def string_storing(text, string_store, list, inc):
     text = text.reset_index(drop=True)
     for i in range(1, len(text.index)):
         doc = nlp(text[i - 1])
         for token in doc:
             if (token.text in [x.text for x in doc.ents]) or (
-                str(token.tag_) in ["SYM", "X", "NUM"]
+                str(token.tag_).upper() in ["SYM", "X", "NUM"]
             ):
                 continue
-            elif str(token.tag_) == "PUNCT" and (
+            elif str(token.tag_).upper() == "PUNCT" and (
                 token.text not in string_store.get("PUNCT")
             ):
                 string_store.get("PUNCT").append(token.text)
             elif token.text[0].lower() not in string_store.keys():
-                string_store[token.text[0].lower()] = list_manager.list()
+                string_store[token.text[0].lower()] = list[inc.value]
+                inc.value += 1
             else:
                 string_store.get(token.text[0].lower()).append(token.text.lower())
                 string_store.get(token.text[0].lower()).sort()
@@ -57,28 +57,35 @@ def string_storing(text, string_store):
 
 
 def csv_to_json(fileName):
-    manager_ = multiprocessing.Manager()
-    string_store = manager_.dict({"PUNCT": []})
+    inc = multiprocessing.Value("i", 0)
+    with multiprocessing.Manager() as manager_, multiprocessing.Manager() as list_manager:
+        list_of_managers = [list_manager.list() for i in range(0, 100)]
+        string_store = manager_.dict()
+        string_store["PUNCT"] = list_of_managers[0]
+        inc.value = 1
+        data_csv = pd.read_csv(fileName)
+        text_column = tagger(data_csv)
+        nm_datasets = math.floor(len(data_csv.index) / cpu_count())
+        processes = []
+        rows = nm_datasets
 
-    data_csv = pd.read_csv(fileName)
-    text_column = tagger(data_csv)
-    nm_datasets = math.floor(len(data_csv.index) / cpu_count())
-    processes = []
-    rows = nm_datasets
-
-    #  side note issue might be because of manager item containing same manager item or creation of manager.list() item inside the process
-    for subprocess in range(0, cpu_count()):
-        processes.append(
-            Process(
-                target=string_storing,
-                args=(text_column[rows - nm_datasets : rows], string_store,),
+        for subprocess in range(0, cpu_count()):
+            processes.append(
+                Process(
+                    target=string_storing,
+                    args=(
+                        text_column[rows - nm_datasets : rows],
+                        string_store,
+                        list_of_managers,
+                        inc,
+                    ),
+                )
             )
-        )
-        rows += nm_datasets
-    for process in processes:
-        process.start()
-    for subprocess in processes:
-        subprocess.join()
+            rows += nm_datasets
+        for process in processes:
+            process.start()
+        for subprocess in processes:
+            subprocess.join()
 
-    print(string_store)
+        print(string_store)
 

55bf0a380e405d6b8110c148dbf2f452f869e0b0
terrance74liang <terrance.liang74@gmail.com>
2022-07-27 01:30:00 -0400

    tested distributions file, still need to fix non single letter represent, but frequency distribution works

--- distributions.py
+++ distributions.py
@@ -6,13 +6,32 @@ import numpy as np
 def is_slice(data, num, shared):
     data = list(data["words"])
     count = [0] * len(data)
+    length = iter(range(0, len(data)))
+    pointer = np.nan
+    for x in data:
+        if len(x) <= 1:
+            pointer = next(length, None)
+        if pointer == None:
+            return None
     for words in range(0, len(data)):
         for comp in range(0, len(data)):
             if words == comp:
                 continue
             if data[words] in data[comp]:
                 count[words] += 1
-    shared.append((data[count.index(max(count))], num))
+    # condition = True
+    # word_index = None
+    # while condition:
+    #     for word, count_freq, index in zip(data, count, range(0, len(data))):
+    #         if count_freq == max(count) and len(word) <= 2:
+    #             count[index] = 0
+    #     for word, count_freq, index in zip(data, count, range(0, len(data))):
+    #         if count_freq == max(count) and len(word) <= 2:
+    #             break
+    #         if index == len(data):
+    #             condition = False
+
+    shared.append((data[word_index], num))
 
 
 def parallel_distributions(num_process, data):

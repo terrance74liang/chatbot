c96f5c4cd10215ac6134ac8b696f3435656930ef
terrance74liang <terrance.liang74@gmail.com>
2022-08-25 02:27:15 -0400

    first test for lstm

--- sentence_generator.py
+++ sentence_generator.py
@@ -7,13 +7,6 @@ from database import pos_extractor
 from sklearn.model_selection import train_test_split
 import functools as ft
 
-# training pos structure from dialogue 1 then 2. input: pos person 1, output: pos person 2
-# sentence subject. input: sentence 1 subject reply, output: sentence 2 subject reply
-# train the inverse to be able to classify reply vs answer
-# markov sentence generator to generate x length setences and appropriate length
-
-# cut setence length then classify, use markov chains to generate the text again
-
 
 def encoder(data):
     pos_tags_spacy_glossary = (
@@ -75,21 +68,6 @@ def t_plus_encoder(data, point):
         raise ValueError("need proper point value")
 
 
-data = (
-    pd.read_csv("casual_data_windows.csv", index_col=0, nrows=1000, dtype="string")
-    .iloc[:, 0:2]
-    .astype("string")
-)
-
-conversational_data = (
-    data.applymap(
-        func=lambda x: sequence_encoder(x)[0:6] if len(x.split()) >= 6 else np.nan
-    )
-    .dropna(how="any", axis=0)
-    .reset_index(drop=True)
-)
-
-
 def martix_creation(data, direction):
     input_matrix = None
     output_matrix = None
@@ -117,10 +95,50 @@ def martix_creation(data, direction):
         raise ValueError("need proper point value")
 
 
-# xtrain, xtest, ytrain, ytest = train_test_split(
-#     conversational_data.iloc[:, 0],
-#     conversational_data.iloc[:, 1],
-#     train_size=0.8,
-#     random_state=42,
-# )
+def seq2seq_model(encoder_input, decoder_input, decoder_target, batch_size, epochs):
+    latent_space_dimension = 256
+    encoder_inputs = tf.keras.Input(shape=(None, 6))
+    encoder = tf.keras.layers.LSTM(latent_space_dimension, return_state=True)
+    encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+    encoder_states = [state_h, state_c]
+    decoder_inputs = keras.Input(shape=(None, 6))
+    decoder_lstm = keras.layers.LSTM(
+        latent_space_dimension, return_sequences=True, return_state=True
+    )
+    decoder_outputs, *_ = decoder_lstm(decoder_inputs, initial_state=encoder_states)
+    decoder_dense = keras.layers.Dense(20, activation="softmax")
+    decoder_outputs = decoder_dense(decoder_outputs)
+    model = keras.Model([encoder_inputs, decoder_inputs], decoder_outputs)
+
+    def training():
+        model.compile(
+            optimizer="rmsprop", loss="categorical_crossentropy", metrics=["accuracy"]
+        )
+        model.fit(
+            [encoder_input, decoder_input],
+            decoder_target,
+            batch_size=batch_size,
+            epochs=epochs,
+            validation_split=0.2,
+        )
+
+        return model
+
+    training()
+
+
+data = (
+    pd.read_csv("casual_data_windows.csv", index_col=0, nrows=1000, dtype="string")
+    .iloc[:, 0:2]
+    .astype("string")
+    .applymap(
+        func=lambda x: sequence_encoder(x)[0:6] if len(x.split()) >= 6 else np.nan
+    )
+    .dropna(how="any", axis=0)
+    .reset_index(drop=True)
+)
+
+xtrain, xtest, ytrain, ytest = train_test_split(
+    data.iloc[:, 0], data.iloc[:, 1], train_size=0.8, random_state=42,
+)
 
